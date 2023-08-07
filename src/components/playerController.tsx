6d2320daf8e1aaea76010637fe3360b91639d712
Liukaku <46352428+Liukaku@users.noreply.github.com>
2023-08-07 12:11:58 +0100

    updated readme, added controls text

--- src/components/playerController.tsx
+++ src/components/playerController.tsx
@@ -7,7 +7,7 @@ interface PlayerControllerProps {
 const PlayerController = ({ movePlayer }: PlayerControllerProps) => {
   const btnNoMargin = btnStyle.replace("my-3", "my-0");
   return (
-    <div className=" shadow-black/30 sticky bottom-10 md:w-1/6 w-1/2 md:ml-1 mx-auto mt-16  border-2 pb-10 bg-gray-300 border-b-gray-500 border-l-gray-500 border-r-gray-100 border-t-gray-100 shadow-xl">
+    <div className=" shadow-black/30 sticky bottom-10 md:w-1/6 w-1/2 md:ml-1 mx-auto mt-16  border-2 md:pb-5 pb-1 bg-gray-300 border-b-gray-500 border-l-gray-500 border-r-gray-100 border-t-gray-100 shadow-xl">
       <div className="p-1 text-xl text-white mb-5 headerBarGrey leading-none bg-zinc-300 border-t-zinc-200 border-r-zinc-200 border-l-zinc-400 border-b-zinc-400">
         <h1 className="select-none">Controls.exe</h1>
       </div>
@@ -37,6 +37,9 @@ const PlayerController = ({ movePlayer }: PlayerControllerProps) => {
           Down
         </button>
       </div>
+      <p className="text-black text-center my-2">
+        (or use the arrow keys on your keyboard)
+      </p>
     </div>
   );
 };
